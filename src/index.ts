b27c4bc58f9dbe413095d62201e216166fd678fe
jj0han <joaovieirabomfim@gmail.com>
2022-09-10 14:59:41 -0400

    Função de deletar item adicionada. Pequenas mudanças de design.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import {v4 as uuidV4} from "uuid"
+import icon from "./assets/images/trash-con.svg"
 
 const list = document.querySelector<HTMLLIElement>("#list")
 const form = document.querySelector<HTMLFormElement>("#new-task-form")
@@ -37,9 +38,16 @@ function addNewTask(newTask : Task) {
   const listItem = document.createElement("li")
   const checkbox = document.createElement("input")
   const label = document.createElement("label")
+  const span = document.createElement("span")
+  const img = document.createElement("img")
+
+  img.classList.add("trash")
+  img.setAttribute("id", newTask.id)
+  img.setAttribute("src", icon)
 
   listItem.classList.add("list-item")
   checkbox.classList.add("checkbox")
+  span.classList.add("txt")
 
   checkbox.addEventListener("change", () => {
     newTask.completed = checkbox.checked
@@ -49,9 +57,16 @@ function addNewTask(newTask : Task) {
   checkbox.type = "checkbox"
   checkbox.checked = newTask.completed
 
-  label.append(checkbox, newTask.title)
+  span.textContent = newTask.title
+  label.append(checkbox, span)
   listItem.appendChild(label)
+  listItem.appendChild(img)
   list?.appendChild(listItem)
+
+  const trash = document.querySelectorAll(".trash")
+  trash?.forEach((item) => {
+    deleteItem(item)
+  })
 }
 
 function saveItems() {
@@ -62,4 +77,46 @@ function getItems() : Task[] {
   const storedItems = localStorage.getItem("TASKS")
   if(storedItems == null) return []
   return JSON.parse(storedItems)
+}
+
+function deleteItem(item: Element) {
+  //adiciona icone de lixeira quando passa o mouse por cima do item
+  if(item === null || item.parentElement === null) return
+  item.parentElement.addEventListener("mouseover", () => {
+    for (let i = 0; i < tasks.length; i++) {
+      if(item.getAttribute("id") === tasks[i].id) {
+        item.classList.add("show")
+      } 
+    }
+  })
+
+  //retira icone de lixeira quando tira o mouse do item
+  item.parentElement.addEventListener("mouseleave", () => {
+    for (let i = 0; i < tasks.length; i++) {
+      if(item.getAttribute("id") === tasks[i].id) {
+        if(item.classList.contains("show")) {
+          item.classList.remove("show")
+        }
+      } 
+    }
+  })
+
+  //remove item e salva array
+  item.addEventListener("click", () => {
+    for (let i = 0; i < tasks.length; i++) {
+      if(item.getAttribute("id") === tasks[i].id) {
+        tasks.splice(i, 1)
+        saveItems()
+      }
+
+      if(item === null || item.parentElement === null) {
+        return
+      } else {
+        item.parentElement.style.opacity = "0"
+      }
+      setTimeout(() => {
+        item.parentElement?.remove()
+      }, 300)
+    }
+  })
 }
